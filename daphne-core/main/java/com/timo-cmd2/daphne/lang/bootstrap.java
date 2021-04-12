2f03d9b4e5e0bcfdc76501a81426638c990a2fa8
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-04-12 11:50:16 +0200

    Update bootstrap.java

--- daphne-core/main/java/com/timo-cmd2/daphne/lang/bootstrap.java
+++ daphne-core/main/java/com/timo-cmd2/daphne/lang/bootstrap.java
@@ -1,1 +1,8 @@
-// init
+package com.timo-cmd2.daphne.lang;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+        String str = readFile(args[0]);
+        Bootstrap.run(str);
+    }
+}
