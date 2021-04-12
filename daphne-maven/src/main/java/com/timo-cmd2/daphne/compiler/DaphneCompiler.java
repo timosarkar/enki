b0de737b32b905d3672511c8bd6a672514d2808f
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-04-12 11:54:03 +0200

    initializing maven compiler

---
+++ daphne-maven/src/main/java/com/timo-cmd2/daphne/compiler/DaphneCompiler.java
@@ -0,0 +1,18 @@
+package com.timo-cmd2.daphne.compiler
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DaphneCompiler {
+
+}
