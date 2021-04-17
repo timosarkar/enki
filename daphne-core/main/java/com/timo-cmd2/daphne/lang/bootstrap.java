b389a2a4f356e01ca25faaf3e57debe4c79ebd72
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-04-17 11:41:33 +0200

    Update and rename daphne-core/main/java/com/timo-cmd2/daphne/lang/bootstrap.java to daphne-core/daphne/lang/package-info.java

--- daphne-core/main/java/com/timo-cmd2/daphne/lang/bootstrap.java
+++
@@ -1,8 +0,0 @@
-package com.timo-cmd2.daphne.lang;
-
-public class Bootstrap {
-    public static void main(String[] args) {
-        String str = readFile(args[0]);
-        Bootstrap.run(str);
-    }
-}
