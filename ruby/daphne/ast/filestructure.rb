dc739ed37440e9f41dd59402212c3d3b74e512ff
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-08 14:49:59 +0200

    fixed accessors

--- ruby/daphne/ast/filestructure.rb
+++ ruby/daphne/ast/filestructure.rb
@@ -1,10 +1,10 @@
 module Daphne
   class Filestructure
-    @pos      = nil,
-    @row      = nil,
-    @column   = nil,
-    @length   = nil,
-    @filename = nil
+    attr_accessor :pos
+    attr_accessor :row
+    attr_accessor :column
+    attr_accessor :length
+    attr_accessor :filename
     
     def initialize(@pos = 0, @row = 0, @column = 0, @length = 0, @filename = "virtual")
     end
