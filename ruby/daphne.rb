6ec8024226ef451ad3352bc6c56065d60e187216
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-11 11:02:02 +0200

    Added requires

--- ruby/daphne.rb
+++ ruby/daphne.rb
@@ -1,6 +1,12 @@
 # v0.1.3-dev
 require "daphne/version"
 
+require "daphne/ast/filestructure"
+require "daphne/ast/tokentype"
+require "daphne/ast/ast"
+
+require "../tools/string-indention"
+
 module Dahne
   def args  = []
   def flags = []
