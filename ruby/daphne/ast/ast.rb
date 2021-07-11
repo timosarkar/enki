d24ecf278fd1bf93a1079e5623c8154aa8f70902
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-11 11:00:04 +0200

    Adding ast file

---
+++ ruby/daphne/ast/ast.rb
@@ -0,0 +1,12 @@
+# main ast walking file
+require "../../../tools/string-indention"
+require "./tokentype"
+require "./filestructure"
+
+module Daphne::AST
+  class ASTnode
+    def node
+      self
+    end
+  end
+end
